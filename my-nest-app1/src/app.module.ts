1e21bd179997da92da260ab01eebcd3452d8bf7e
bhushan-ai <bhushaningole2004@gmail.com>
2026-03-07 14:11:52 +0530

    dependency injection DI done

--- my-nest-app1/src/app.module.ts
+++ my-nest-app1/src/app.module.ts
@@ -5,9 +5,10 @@ import { UserController } from './user/user.controller';
 import { ProductsService } from './products/products.service';
 import { ProductsController } from './products/products.controller';
 import { EmployeeModule } from './employee/employee.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [EmployeeModule, CategoryModule],
   controllers: [AppController, UserController, ProductsController],
   providers: [AppService, ProductsService],
 })
